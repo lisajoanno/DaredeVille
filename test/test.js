dbd0ece127b38dd4828164a2e9814542c7e914a1
PierreMassanes <pierre.massanes@gmail.com>
2016-11-04 11:45:32 +0100

    Added mocha script to package.json

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
 var chai = require('chai');
 var expect = chai.expect;
+var directionDriver = require('../components/directions/itinerary_calculator').directionDriver;
 
+describe('Itinerary', function() {
+    it('Direction driver', function() {
+        directionDriver.getItinerary().then(function(data){
+            expect(data.status).to.equal("OK");
+        });
+    });
+});
