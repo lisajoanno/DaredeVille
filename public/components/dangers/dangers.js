9ed67c81b9b51e1b4ab9d0b736a5323f21a3fde7
Alexandre CAZALA <alexandre.cazala@gmail.com>
2016-11-04 11:59:29 +0100

    première version IHM

---
+++ public/components/dangers/dangers.js
@@ -0,0 +1,14 @@
+/************************************************
+ * PROJECT:        DardeVille                   *
+ * AUTHOR:         Alexandre Cazala             *
+ * CREATION_DATE:  04/11/16                      *
+ * EMAIL:          alexandre.cazala@gmail.com   *
+ * LICENSE:        Apache 2.0                   *
+ ***********************************************/
+'use strict';
+
+var app = angular.module('dardeville.dangers', ['ngRoute']);
+
+app.controller('dangersCtrl', ['$scope', '$http',function($scope, $http) {
+    console.log("dangersCtrl");
+}]);
---
+++ public/components/itinerary/itinerary.js
@@ -0,0 +1,14 @@
+/************************************************
+ * PROJECT:        DardeVille                   *
+ * AUTHOR:         Alexandre Cazala             *
+ * CREATION_DATE:  04/11/16                      *
+ * EMAIL:          alexandre.cazala@gmail.com   *
+ * LICENSE:        Apache 2.0                   *
+ ***********************************************/
+'use strict';
+
+var app = angular.module('dardeville.itinerary', ['ngRoute']);
+
+app.controller('itineraryCtrl', ['$scope', '$http',function($scope, $http) {
+    console.log("itineraryCtrl");
+}]);
