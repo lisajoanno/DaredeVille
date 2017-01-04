0515a8a709a96e053b1806866138381bb158398a
PierreMassanes <pierre.massanes@gmail.com>
2017-01-04 16:03:35 +0100

    Trying to mock database

--- test/testDangers.js
+++ test/testDangers.js
@@ -1,6 +1,8 @@
 var chai = require('chai');
 var expect = chai.expect;
 var request = require("request");
+var mockDb = require('mock-knex');
+var db = require('../model/connection').knex;
 
 var url = "http://daredeville.herokuapp.com/api/v1/dangers";
 
@@ -8,9 +10,14 @@ describe('Dangers', function () {
 
     describe('Test GET dangers route', function () {
         it('returns status 200', function (done) {
+            mockDb.mock(db);
+
             request(url, function (error, response, body) {
                 expect(response.statusCode).to.equal(200);
+                //expect(true).to.be.true;
             });
+
+            mockDb.unmock(db);
         })
     });
 
