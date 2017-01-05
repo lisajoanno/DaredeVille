81040ada7009c8f41d58eb8ff1596e6adf1763a6
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-01-06 00:23:18 +0100

    Displaying position

--- routes/position.js
+++ routes/position.js
@@ -6,11 +6,11 @@ var User = require('../model/models').User;
 
 var jsonfile = require('jsonfile');
 
+var file = 'sorry.json';
 router.post('/', function (req, res, next) {
-    console.log('Received position');
 
+    console.log('Received position');
     /* Sorry for the following lines, but I would waste way too much time dealing with knex */
-    var file = 'sorry.json';
     position = req.body;
     jsonfile.readFile(file, function(err, obj) {
         position.date = Date.now();
