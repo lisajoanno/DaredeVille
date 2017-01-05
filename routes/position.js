c98b8172fd2c70d9d8e7a46766e3ad97f5946484
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-01-05 10:59:56 +0100

    Added positions table and insert rows in it when it's sent a new position by the android device

--- routes/position.js
+++ routes/position.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var Position = require('../model/models').Position;
+var User = require('../model/models').User;
 
 
-router.post('/', function(req, res, next) {
-    console.log("Reveived position")
+router.post('/', function (req, res, next) {
+    console.log('Received position');
+    User.where({
+        email: 'alexandre.cazala@gmail.com'
+    }).select('id').then(function (a, b, c) {
+        console.log('Result of select query:');
+        console.log(a, b, c);
+        Position.insertRow({
+            user_id: 1,
+            latitude: 16151651,
+            longitude: 5464561
+        }).then(function(a, b, c) {
+            console.log("New position inserted for user something");
+        })
+    });
 });
