55f964b41dc8446935803e94185a2d1d36c54cc4
PierreMassanes <pierre.massanes@gmail.com>
2017-01-19 18:29:38 +0100

    Removing done callback...

--- test/testItinerary.js
+++ test/testItinerary.js
@@ -9,12 +9,11 @@ var url = "http://daredeville.herokuapp.com/api/v1/itinerary";
 describe('Itinerary', function() {
 
     describe('Direction driver', function () {
-        it('gets the itinerary', function(done) {
+        it('gets the itinerary', function() {
             return directionDriver.getItinerary().then(function(data){
                 expect(data).not.to.be.undefined;
                 expect(data.routes).not.to.be.undefined;
                 expect(data.status).to.be.undefined;
-                done();
             });
         });
     });
