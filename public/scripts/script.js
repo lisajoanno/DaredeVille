3e6d35923c3b7bd573947591abb147cb8e086a88
Alexandre CAZALA <alexandre.cazala@gmail.com>
2017-02-02 18:31:38 +0100

    [DEV] carte des aveugles

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -125,10 +125,23 @@ function deleteMarkers() {
 }
 function updatePositions() {
     $.get( "/api/v1/position", function( data ) {
+        // data = data.reverse().slice(0, 5);
+        // data.forEach(function(location) {
+        //     updatePosition({lat:location.lat, lng:location.lng}, location.email, location.email);
+        // });
         data = data.reverse().slice(0, 5);
-        data.forEach(function(location) {
-            updatePosition({lat:location.lat, lng:location.lng}, location.email, location.email);
+        data.forEach(function(position) {
+        console.log(position);
+        var pos = {lat: position.lat, lng: position.lng};
+        var date = new Date(position.date*1000);
+
+        var marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            title: position.email
         });
+            markers.push(marker)
+        })
     });
 }
 function position() {
